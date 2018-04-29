5010e268ed6481eae99c6dac727d8c6bd40c7ddd
aginwala <aginwala@ebaysf.com>
2018-04-29 00:33:10 -0700

    fix pip10 compatibility in setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""setup.py file."""
+import uuid
+
+from setuptools import setup, find_packages
+#from pip.req import parse_requirements
+
+__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+
+#install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+#reqs = [str(ir.req) for ir in install_reqs]
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+
+setup(
+    name="napalm-base",
+    version='0.21.0',
+    packages=find_packages(),
+    author="David Barroso",
+    author_email="dbarrosop@dravetech.com",
+    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    classifiers=[
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+    ],
+    url="https://github.com/napalm-automation/napalm-base",
+    include_package_data=True,
+    install_requires=parse_requirements('requirements.txt'),
+    entry_points={
+        'console_scripts': [
+            'cl_napalm_configure=napalm_base.clitools.cl_napalm_configure:main',
+            'cl_napalm_test=napalm_base.clitools.cl_napalm_test:main',
+            'cl_napalm_validate=napalm_base.clitools.cl_napalm_validate:main'
+        ],
+    }
+)
